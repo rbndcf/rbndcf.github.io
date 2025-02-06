491413573f4887bf2e8f62b2254d16090886e9da
Rubén Del Castillo Fuentes <ruben.dcf14@gmail.com>
2025-02-06 11:44:59 +0100

    Pasando HTML de main.js a index.html

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const appDiv = document.querySelector(".app");
     if (appDiv) {
-        appDiv.textContent = "Rubén";
+        appDiv.innerHTML = `
+        <h1> Rubén Del Castillo Fuentes </h1>
+        <h2> Desarrollador Software </h2>
+        `;
     }
 });
